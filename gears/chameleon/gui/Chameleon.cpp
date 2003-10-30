75776d43d43ae4d0d1cb801eb480db364656cda6
Mark Erikson <mark@isquaredsoftware.com>
2003-10-30 05:33:39 +0000

    Updated funtions to take wxCommandEvent parameters.  Added splitter and terminal textbox.

--- gears/chameleon/gui/Chameleon.cpp
+++ gears/chameleon/gui/Chameleon.cpp
@@ -36,11 +36,13 @@
 #include <wx/filedlg.h>
 #include <wx/file.h>
 #include <wx/textfile.h>
+#include <wx/splitter.h>
 
 #include "new.xpm"
 #include "open.xpm"
 #include "save.xpm"
 #include "build.xpm"
+#include "button.xpm"
 
 #include "stc.h"
 
@@ -62,12 +64,23 @@ public:
 
     void OnQuit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
-	void SaveFile();
-	void OpenFile();
+	void SaveFile(wxCommandEvent& event, bool saveas);
+	void OpenFile(wxCommandEvent& event);
+	void Test(wxCommandEvent& event);
+	void SaveFileAs(wxCommandEvent& event);
+
 	
 private:
 	void CheckSize();
+	void ResizeSplitter();
+
+
+
     wxStyledTextCtrl* ed;
+	wxSplitterWindow* split;
+	wxTextCtrl* textbox;
+
+	wxString saveFileName;
 
     DECLARE_EVENT_TABLE()
 };
@@ -77,18 +90,29 @@ private:
 enum
 {
     // menu items
-    ID_Quit = 1,
+    ID_Quit = 5200,
+	ID_New,
+	ID_Open,
+	ID_Save,
     ID_About,
-    ID_ED
+    ID_ED,
+	ID_Copy,
+	ID_Cut,
+	ID_Paste,
+	ID_Compile,
+	ID_Test,
 };
 
 BEGIN_EVENT_TABLE(MyFrame, wxFrame)
+	EVT_MENU			(ID_Open, MyFrame::OpenFile)
     EVT_MENU            (ID_Quit,  MyFrame::OnQuit)
     EVT_MENU            (ID_About, MyFrame::OnAbout)
-	EVT_MENU			(5204, MyFrame::CheckSize)
-	EVT_MENU			(5203, MyFrame::SaveFile)
-	EVT_MENU			(5202, MyFrame::OpenFile)
+	EVT_MENU			(ID_Test, MyFrame::ResizeSplitter)
+	EVT_MENU			(ID_Save, MyFrame::SaveFile)
+	//
+	//EVT_SIZE			(MyFrame::ResizeSplitter)
 END_EVENT_TABLE()
+
 
 IMPLEMENT_APP(MyApp)
 
@@ -116,6 +140,8 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
     wxApp::s_macAboutMenuItemId = ID_About;
 #endif
 
+	this->SetClientSize(640, 480);
+
 
     // create a menu bar
     wxMenu *menuFile = new wxMenu("", wxMENU_TEAROFF);
@@ -124,6 +150,10 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
     wxMenu *helpMenu = new wxMenu;
     helpMenu->Append(ID_About, "&About...\tCtrl-A", "Show about dialog");
 
+	menuFile->Append(ID_New, "&New\tCtrl-N", "Create a new file");
+	menuFile->Append(ID_Open, "&Open\tCtrl-O", "Open an existing file");
+	menuFile->Append(ID_Save, "&Save\tCtrl-S", "Save the current file");
+
     menuFile->Append(ID_Quit, "E&xit\tAlt-X", "Quit this program");
 
     // now append the freshly created menu to the menu bar...
@@ -131,36 +161,86 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
     menuBar->Append(menuFile, "&File");
     menuBar->Append(helpMenu, "&Help");
 
+	
 	wxToolBar* toolBar = CreateToolBar(wxTB_FLAT | wxTB_TEXT);
 	wxBitmap bmNew(new_xpm);
-	toolBar->AddTool(5201, "New File", bmNew);
-
+	toolBar->AddTool(ID_New, "New File", bmNew);
+	
+		
 	wxBitmap bmOpen(open_xpm);
-	toolBar->AddTool(5202, "Open File", bmOpen);
+	toolBar->AddTool(ID_Open, "Open File", bmOpen);
 
 	wxBitmap bmSave(save_xpm);
-	toolBar->AddTool(5203, "Save File", bmSave);
+	toolBar->AddTool(ID_Save, "Save File", bmSave);
+
+	toolBar->InsertSeparator(3);
 
 	wxBitmap bmBuild(build_xpm);
-	toolBar->AddTool(5204, "Compile", bmBuild);
+	toolBar->AddTool(ID_Compile, "Compile", bmBuild);
+
+	toolBar->InsertSeparator(5);
+
+	wxBitmap bmTest(button_xpm);
+	toolBar->AddTool(ID_Test, "Test", button_xpm);
+	
+
 
 
 
 	toolBar->Realize();
 
 	SetToolBar(toolBar);
+	
     // ... and attach this menu bar to the frame
     SetMenuBar(menuBar);
 
 #if wxUSE_STATUSBAR
     CreateStatusBar(2);
-    SetStatusText("Testing wxStyledTextCtrl");
+    SetStatusText("Statusbar text");
 #endif // wxUSE_STATUSBAR
+
+
+	split = new wxSplitterWindow(this, 5205);
+	
+
+
 
 
     //----------------------------------------
     // Setup the editor
-    ed = new wxStyledTextCtrl(this, ID_ED);
+    ed = new wxStyledTextCtrl(split, ID_ED);
+
+
+
+	textbox = new wxTextCtrl(split, 5206, "", wxDefaultPosition, wxDefaultSize,
+		wxTE_MULTILINE | wxTE_RICH, wxDefaultValidator);
+	ed->SetMarginWidth(0, 20);
+	ed->SetMarginType(0, 1);
+	
+	
+	wxColour black("black");
+	wxColour white("white");
+	wxFont f;
+	f.SetFaceName("Courier New");
+	f.SetFamily(wxMODERN);
+	f.SetPointSize(12);
+	wxTextAttr ta(white, black, f);
+	textbox->SetDefaultStyle(ta);
+	textbox->SetBackgroundColour(black);
+	
+	textbox->SetForegroundColour(white);
+	
+	
+	
+
+	
+	
+	*textbox << "[s1278644@john ~]$ ";
+	textbox->SetStyle(0, textbox->GetValue().Length(), ta);
+
+
+	split->SplitHorizontally(ed, textbox, 500);
+	split->SetMinimumPaneSize(140);
 
     // Default font
     wxFont font(10, wxMODERN, wxNORMAL, wxNORMAL);
@@ -211,16 +291,37 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
                     "public register reinterpret_cast return short signed "
                     "sizeof static static_cast struct switch template this "
                     "throw true try typedef typeid typename union unsigned "
-                    "using virtual void volatile wchar_t while");
-	
+                    "using virtual void volatile wchar_t while");	
+}
 
-	
+void MyFrame::OpenFile(wxCommandEvent& WXUNUSED(event))
+{
+	wxString filetext;
+	wxString filename = ::wxFileSelector("Pick a file", "",
+		"", "", "", wxOPEN, NULL);
+	if(!filename.empty())
+	{
 
-	
+		wxFile file(filename);
+		wxString st;
+
+		char* buff = st.GetWriteBuf(file.Length());
+		file.Read(buff, file.Length());
+		st.UngetWriteBuf();
+
+		ed->InsertText(0, st);
+
+		saveFileName = filename;		
+	}
 }
 
 
 // event handlers
+void MyFrame::Test(wxCommandEvent& WXUNUSED(event))
+{
+	int i = 0;
+}
+
 
 void MyFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
 {
@@ -248,56 +349,67 @@ void MyFrame::CheckSize()
 	
 }
 
-void MyFrame::OpenFile()
+
+
+void MyFrame::SaveFile(wxCommandEvent& WXUNUSED(event), bool saveas)
 {
-	wxString filetext;
-	wxString filename = ::wxFileSelector("Pick a file", "",
-		"", "", "", wxOPEN, NULL);
-	if(!filename.empty())
+
+	wxString name = wxEmptyString;
+
+	/*
+	if( (saveFileName == wxEmptyString) ||
+		(saveas) )
 	{
-		wxTextFile file(filename);
-		file.Open();
-		int numlines = file.GetLineCount();
+		SaveFileDialog sfd = new SaveFileDialog();
+		sfd.Filter = "HTML files (*.html)|*.html|All files (*.*)|*.*";
+		DialogResult dr = sfd.ShowDialog();
 
+		if(dr == DialogResult.OK)
+		{	
+			name  = sfd.FileName;
 
-		
-		for (wxString str = file.GetFirstLine(); !file.Eof(); str = file.GetNextLine() )
-		{
-			filetext << str << "\n";
+			if(sfd.FilterIndex == 1)
+			{
+				int idx = name.LastIndexOf(".");
+				string justname = name.Substring(0, idx);
+				name = justname + ".html";
+			}	
 		}
-
-		filetext << file.GetLastLine();
-		
-		/*
-		for(int i = 0; i < file.GetLineCount(); i++)
+		else
 		{
-			filetext << file[i];
+			return;
 		}
-		*/
-		//filetext = file[3];
-		//filetext << file[4];
-
-		//filetext << file.GetLastLine();
-
-
-
-		wxString length;
-		length << "Length: ";
-		length << filetext.Length();
-		ed->SetText(filetext);
-		::wxMessageBox(length);
-		file.Close();
 	}
-}
+	else
+	{
+		name = saveFileName;
+	}			
 
-void MyFrame::SaveFile()
-{
+	StreamWriter sw = new StreamWriter(name);
+	sw.WriteLine(ed.TextBox.Text);			
+	sw.Close();
+	ed.TextBox.Modified = false;
+
+
+*/
+
+
+
+
+
+
+	//if(ed->GetModify())
 	wxString filetext;
 	wxString filename = ::wxFileSelector("Pick a file", "",
 		"", "", "", wxSAVE, NULL);
 	if(!filename.empty())
 	{
+		wxFile file(filename);
+
+		file.Write(ed->GetText());
+		/*
 		wxTextFile file(filename);
+		file.Read
 		if(!file.Exists())
 		{
 			file.Create();
@@ -307,5 +419,29 @@ void MyFrame::SaveFile()
 		file.AddLine(buffertext);
 		file.Write();
 		file.Close();
+		*/
 	}
 }
+
+void MyFrame::SaveFileAs(wxCommandEvent& WXUNUSED(event))
+{
+
+}
+
+
+
+
+void MyFrame::ResizeSplitter()
+{
+	int x, y;
+	this->GetClientSize(&x, &y);
+	int newsize = (y / 7) * 6;
+
+	
+
+	split->SetSashPosition(newsize);	
+	
+}
+
+
+
