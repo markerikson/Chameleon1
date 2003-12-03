81f9e16897aafb7bfa4ab5d61b7243d2943ac027
Ben Carhart <ben@example.com>
2003-12-03 02:43:43 +0000

    c0d3_m0nk3y>>updated per Nov 30

--- gears/chameleon/perms/p.cpp
+++ gears/chameleon/perms/p.cpp
@@ -1,182 +1,229 @@
 /*******************\
 *Permissions Manager*
 \*******************/
+//Source code for the permissions manager header file, created for the
+//Chameleon Flex IDE.
+//Author: Ben Carhart
+//Date started: 11/22/03
+//Date officially completed: --/--/--
+//Description: manages the handling of permissions
+//Major revisions:
+//  ~BSC--xx/xx/xx--Changed the loading of the permCode by using a temp
+//                  bitset variable
+//  ~BSC--xx/xx/xx--Altered the error handling of the program to be
+//                  a little more graceful.
+//  ~BSC--11/29/03--Added the authorization bitset variable & implementation
 
+//includes
 #include "p.h"
 #include<fstream>
 #include<string>
 #include<iostream>
 
+//global variables
 using namespace std;
 
 const string PERM_FILE = "perm.dat";
 const int NOISE = 42;
 
+//@@@@@@@@@
+//@ BEGIN @
+//@@@@@@@@@
+
+//Constructor
+//Input: Zip right now.
+//Output: Nothing really; just initializes the authorization and permission
+//        codes and arrays.
 Permission::Permission()
 {
-	long fuzz;
+	int fuzz, topaz;
 
 	//load the perm level
 	ifstream in;
 	in.open(PERM_FILE.c_str());
 
-	in>>fuzz>>fuzz>>permCode>>fuzz;
+	if(!in)
+	{
+		io = false;
+		permCode = 0;
+	}
+	else
+	{
+		io = true;
+		in>>fuzz>>fuzz>>permCode>>fuzz;
+		in>>topaz>>authCode>>topaz>>topaz;
+	}
+	
 	in.close();
 
-	//decode the permCode (oooh secure)
+	//decode the codes (oooh secure)
 	permCode = (permCode / fuzz) + fuzz;
 	fuzz = permCode;
 
-	cout<<"--PERMISSIONS::CONSTRUCTOR fuzz = "<<fuzz<<" |permCode = "<<permCode<<endl;
+	authCode = (authCode + (topaz * topaz)) / (-topaz);
+	topaz = authCode;
+
+	//DEBUG CODE//
+	//cout<<"--PERMISSIONS::CONSTRUCTOR fuzz = "<<fuzz<<" |permCode = "<<permCode<<endl;
 
 	//load permission code into bitset Array
-	//after reading codumentation the following code is unneccesary
-	//load up the status "array" using this method:
-	/*
-		2|134 = 0
-		 | 67 = 1
-		 | 33 = 1
-		 | 16 = 0
-		 |  8 = 0
-		 |  4 = 0
-		 |  2 = 0
-		    1
-			
-		10000110 = 134
-	*//*
-	for(int i = 0; i < NUM_MODULES; i++)
-	{
-		if(permCode == 1)
-		{
-			status.set(i);
-			i = NUM_MODULES;
-		}
-		else if(permCode % 2 == 1)
-		{
-			status.set(i);
-		}
-		else
-		{
-			status.reset(i);
-		}
 
-		permCode = long(permCode / 2);
-	}
-	*/
-	//this code accomplishes the same thing:
-	bitset<NUM_MODULES> temp(permCode);
-	status = temp;
+	bitset<NUM_MODULES> tempP(permCode);
+	bitset<NUM_MODULES> tempA(authCode);
+	status = tempP;
+	auth = tempA;
 
-	cout<<"--PERMISSIONS::CONSTRUCTOR temp = "<<temp<<endl;
-
+	//DEBUG CODE//
+	//cout<<"--PERMISSIONS::CONSTRUCTOR temp = "<<temp<<endl;
 
 	permCode = status.to_ulong();
+	authCode = auth.to_ulong();
 
-	if(fuzz != permCode)
+	if(permCode != fuzz)
 	{
+		/* Old Error Handling
 		cout<<"ERROR: Perm code did not de-code into Bitset"<<endl
 			<<"Bitset: "<<permCode<<endl
 			<<"Code:   "<<fuzz<<endl;
 		exit(1);
+		*/
+		//error has occured decoding the permissions code.Reset to null state
+		status.reset();
+	}
+
+	if(authCode != topaz)
+	{
+		auth.reset();
 	}
 
 	//everything loaded correctly!
 }
 
+//Destructor
+//Kills everything.  Also handles saving of the permissions and authorization
+//codes
 Permission::~Permission()
 {
 	ofstream fout;
 	fout.open(PERM_FILE.c_str());
 
-	//encode the permCode
-	permCode = (permCode - NOISE) * NOISE;
+	//unable to generate output file.
+	if(!fout)
+	{
+		io = false;
+	}
+	else
+	{
+		//output file generated.
+		io = true;	//*pointless really*//
 
-	fout<<NOISE + NOISE<<endl
-		<<NOISE / (NOISE - (NOISE / 2))<<endl
-		<<permCode<<endl
-		<<NOISE<<endl;
+		//encode the codes
+		permCode = (permCode - NOISE) * NOISE;
+		authCode = (authCode * (NOISE - 15)) - ((NOISE - 15) * (NOISE - 15));
+
+		fout<<NOISE + NOISE<<endl
+			<<NOISE / (NOISE - (NOISE / 2))<<endl
+			<<permCode<<endl
+			<<NOISE<<endl
+			<<(NOISE * NOISE)<<endl
+			<<authCode<<endl
+			<<(NOISE * (NOISE - 15))<<endl
+			<<(NOISE - 15)<<endl;
+	}
 
 	fout.close();
 
-	cout<<"--PERMISSIONS::DESTRUCTOR permCode = "<<permCode<<endl;
+	//DEBUG CODE//
+	//cout<<"--PERMISSIONS::DESTRUCTOR permCode = "<<permCode<<endl;
 
 	status.reset();
+	auth.reset();
 	permCode = 0;
+	authCode = 0;
 }
 
+//fileSuccess
+//Input: nothing
+//Output: returns whether the permissions file was successfully parsed.
+bool Permission::fileSuccess()
+{
+	return(io);
+}
 
+//isEnabled
+//Input: a valid module ID
+//Output: a boolean TRUE if the [status] bit is set, FALSE otherwise
 bool Permission::isEnabled(int id)
 {
 	return(status.test(id));
 }
 
+//isAuthorized
+//Input: a valid module ID
+//Output: a boolean TRUE if the [auth] bit is set, FALSE otherwise
+bool Permission::isAuthorized(int id)
+{
+	return(auth.test(id));
+}
+
+//enable
+//Input: a valid module ID
+//Output: nothing.  [status] and [permCode] are updated.
 void Permission::enable(int id)
 {
 	status.set(id);
 	permCode = status.to_ulong();
 }
 
+//disable
+//input: a valid module ID
+//output: nothing.  [status] and [permCode] are updated
 void Permission::disable(int id)
 {
 	status.reset(id);
 	permCode = status.to_ulong();
 }
 
+//setGlobal
+//input: a valid authorization code
+//output: TRUE if the code parsed correctly, FALSE otherwise.  [auth] and
+//        [authCode] are updated.
 bool Permission::setGlobal(long code)
 {
 	long int fuzz = code;
 
-	//after reading documentation, the following code is unneccesary
-	//load up the status "array" using this method:
-	/*
-		2|134 = 0
-		 | 67 = 1
-		 | 33 = 1
-		 | 16 = 0
-		 |  8 = 0
-		 |  4 = 0
-		 |  2 = 0
-		    1
-			
-		10000110 = 134
-	*//*
-	for(int i = 0; i < NUM_MODULES; i++)
-	{
-		if(code == 1)
-		{
-			status.set(i);
-			i = NUM_MODULES;
-		}
-		else if(code % 2 == 1)
-		{
-			status.set(i);
-		}
-		else
-		{
-			status.reset(i);
-		}
-
-		code = long(code / 2);
-	}
-	*/
-
-	//this code does the same thing:
+	//load up the authorization array with the new permissions
 	bitset<NUM_MODULES> temp(code);
-	status = temp;
+	auth = temp;
 
-	code = status.to_ulong();
+	//test for successful pass
+	code = auth.to_ulong();
 
-	if(fuzz != code)
+	if(code != fuzz)
 		return(false);
 
-	permCode = code;
+	authCode = code;
 	return(true);
 }
 
+//getGlobal
+//input: (this version) nothing
+//output: the current permissions code
+long Permission::getGlobal()
+{
+	return(permCode);
+}
+
+//getGlobal 2
+//input: a key number, 0 or otherwise
+//output: the authorization code, either straight or from the bitset array
 long Permission::getGlobal(int mode = 0)
 {
 	if(mode == 0)
-		return(status.to_ulong());
+		return(auth.to_ulong());
 
-	return(permCode);
+	return(authCode);
 }
+//@@@@@@@
+//@ END @
+//@@@@@@@
--- gears/chameleon/perms/p.h
+++ gears/chameleon/perms/p.h
@@ -1,47 +1,55 @@
 /*******************\
 *Permissions Manager*
 \*******************/
-/*
-  Revisions:
-
-  Notes:
-*/
+//Header file for the permissions manager for Chameleon Flex IDE
+//Author: Ben Carhart
+//Date started: 11/22/03
+//Date officially completed: --/--/--
+//Description: code prototyping for the permissions class
+//Revisions:
 
 #ifndef PERM___H
 #define PERM___H
 
+//includes
 #include<bitset>
 
+//globals
 using namespace std;
 
 const int NUM_MODULES = 15;
 //give each module a number
-
-
-enum Permissions
+enum modules
 {
-	PERM_SYNTAXHIGHLIGHT = 0,
+	PERM_FIRST = -1,
+	PERM_SYNTAXHIGHLIGHT,
 	PERM_AUTOCOMPLETE,
 	PERM_AUTOINDENT,
 	PERM_DEBUG,
+	PERM_LAST
 };
 
-
-
+//class declaration
 class Permission
 {
 	public:
 		Permission();
 		~Permission();
 		bool isEnabled(int);	//module-specific permissions request
+		bool isAuthorized(int);	//module-specific authorization request
 		void enable(int);		//module-specific enable
 		void disable(int);		//module-specific disable
 		bool setGlobal(long);	//set everything
-		long getGlobal(int);	//status of everything
+		long getGlobal();		//status of everything
+		long getGlobal(int);	//status of everything modular return
+		bool fileSuccess();		//returns TRUE if file I/O was successful
 
 	private:
 		bitset <NUM_MODULES> status;//array for holding module status
+		bitset <NUM_MODULES> auth;	//array for holding what's allowed
 		long permCode;				//int for holding permissions code #
+		long authCode;				//int for holding authorized code #
+		bool io;					//holds status for the I/O operations
 };
 
 #endif
