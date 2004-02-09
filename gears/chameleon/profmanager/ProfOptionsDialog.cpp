bc22988053fbd0ec7bcdd6089c34176c0bcf06fc
Mark Erikson <mark@isquaredsoftware.com>
2004-02-09 01:34:36 +0000

    Initial commit of the ProfOptionsManager code.
    Dialog design should be done.

---
+++ gears/chameleon/profmanager/ProfOptionsDialog.cpp
@@ -0,0 +1,168 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        ProfOptionsDialog.cpp
+// Purpose:     
+// Author:      
+// Modified by: 
+// Created:     02/09/04 19:36:08
+// RCS-ID:      
+// Copyright:   
+// Licence:     
+/////////////////////////////////////////////////////////////////////////////
+
+#ifdef __GNUG__
+#pragma implementation "ProfOptionsDialog.cpp"
+#endif
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+////@begin includes
+#include "wx/wx.h"
+////@end includes
+
+#include "ProfOptionsDialog.h"
+
+////@begin XPM images
+
+////@end XPM images
+
+/*!
+ * ProfOptionsDialog type definition
+ */
+
+IMPLEMENT_CLASS( ProfOptionsDialog, wxDialog )
+
+/*!
+ * ProfOptionsDialog event table definition
+ */
+
+BEGIN_EVENT_TABLE( ProfOptionsDialog, wxDialog )
+
+////@begin ProfOptionsDialog event table entries
+    EVT_BUTTON( ID_GENERATE, ProfOptionsDialog::OnGenerateClick )
+
+    EVT_BUTTON( ID_EXITBUTTON, ProfOptionsDialog::OnExitbuttonClick )
+
+////@end ProfOptionsDialog event table entries
+
+END_EVENT_TABLE()
+
+/*!
+ * ProfOptionsDialog constructors
+ */
+
+ProfOptionsDialog::ProfOptionsDialog( )
+{
+}
+
+ProfOptionsDialog::ProfOptionsDialog( wxWindow* parent, wxWindowID id, const wxString& caption, const wxPoint& pos, const wxSize& size, long style )
+{
+    Create(parent, id, caption, pos, size, style);
+}
+
+/*!
+ * ProfOptionsDialog creator
+ */
+
+bool ProfOptionsDialog::Create( wxWindow* parent, wxWindowID id, const wxString& caption, const wxPoint& pos, const wxSize& size, long style )
+{
+////@begin ProfOptionsDialog member initialisation
+////@end ProfOptionsDialog member initialisation
+
+////@begin ProfOptionsDialog creation
+    wxDialog::Create( parent, id, caption, pos, size, style );
+
+    CreateControls();
+    GetSizer()->Fit(this);
+    GetSizer()->SetSizeHints(this);
+    Centre();
+////@end ProfOptionsDialog creation
+    return TRUE;
+}
+
+/*!
+ * Control creation for ProfOptionsDialog
+ */
+
+void ProfOptionsDialog::CreateControls()
+{    
+////@begin ProfOptionsDialog content construction
+
+    ProfOptionsDialog* item1 = this;
+
+    wxBoxSizer* item2 = new wxBoxSizer(wxVERTICAL);
+    item1->SetSizer(item2);
+    item1->SetAutoLayout(TRUE);
+
+    wxBoxSizer* item3 = new wxBoxSizer(wxHORIZONTAL);
+    item2->Add(item3, 0, wxALIGN_CENTER_HORIZONTAL|wxALL, 5);
+
+    wxBoxSizer* item4 = new wxBoxSizer(wxVERTICAL);
+    item3->Add(item4, 0, wxALIGN_TOP|wxALL, 5);
+
+    wxStaticText* item5 = new wxStaticText( item1, wxID_STATIC, _("Available modules:"), wxDefaultPosition, wxDefaultSize, 0 );
+    item4->Add(item5, 0, wxALIGN_CENTER_HORIZONTAL|wxALL|wxADJUST_MINSIZE, 5);
+
+    wxString* item6Strings = NULL;
+    wxCheckListBox* item6 = new wxCheckListBox( item1, ID_CHECKLISTBOX, wxDefaultPosition, wxSize(200, 240), 0, item6Strings, 0 );
+    m_chklstModules = item6;
+    item4->Add(item6, 0, wxALIGN_CENTER_HORIZONTAL|wxLEFT|wxRIGHT|wxBOTTOM, 5);
+
+    wxBoxSizer* item7 = new wxBoxSizer(wxVERTICAL);
+    item3->Add(item7, 0, wxALIGN_TOP|wxALL, 5);
+
+    wxStaticText* item8 = new wxStaticText( item1, wxID_STATIC, _("Generated activation code:"), wxDefaultPosition, wxDefaultSize, 0 );
+    item7->Add(item8, 0, wxALIGN_LEFT|wxALL|wxADJUST_MINSIZE, 5);
+
+    wxTextCtrl* item9 = new wxTextCtrl( item1, ID_TXTCODE, _(""), wxDefaultPosition, wxSize(140, -1), 0 );
+    m_txtGeneratedCode = item9;
+    item7->Add(item9, 0, wxALIGN_LEFT|wxLEFT|wxRIGHT|wxBOTTOM, 5);
+
+    wxCheckBox* item10 = new wxCheckBox( item1, ID_RANDOMIZE, _("Randomize code"), wxDefaultPosition, wxSize(100, -1), 0 );
+    m_chkRandomize = item10;
+    item10->SetValue(FALSE);
+    item7->Add(item10, 0, wxALIGN_LEFT|wxALL, 5);
+
+    wxButton* item11 = new wxButton( item1, ID_GENERATE, _("Generate code"), wxDefaultPosition, wxSize(90, -1), 0 );
+    m_genButton = item11;
+    item7->Add(item11, 0, wxALIGN_LEFT|wxALL, 5);
+
+    wxButton* item12 = new wxButton( item1, ID_EXITBUTTON, _("Exit"), wxDefaultPosition, wxDefaultSize, 0 );
+    m_exitButton = item12;
+    item7->Add(item12, 0, wxALIGN_RIGHT|wxTOP, 140);
+
+////@end ProfOptionsDialog content construction
+}
+
+/*!
+ * wxEVT_COMMAND_BUTTON_CLICKED event handler for ID_GENERATE
+ */
+
+void ProfOptionsDialog::OnGenerateClick( wxCommandEvent& event )
+{
+    // Insert custom code here
+    event.Skip();
+}
+
+/*!
+ * wxEVT_COMMAND_BUTTON_CLICKED event handler for ID_EXITBUTTON
+ */
+
+void ProfOptionsDialog::OnExitbuttonClick( wxCommandEvent& event )
+{
+    // Insert custom code here
+    event.Skip();
+}
+
+/*!
+ * Should we show tooltips?
+ */
+
+bool ProfOptionsDialog::ShowToolTips()
+{
+  return TRUE;
+}
---
+++ gears/chameleon/profmanager/ProfOptionsDialog.h
@@ -0,0 +1,91 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        ProfOptionsDialog.h
+// Purpose:     
+// Author:      
+// Modified by: 
+// Created:     02/09/04 19:36:08
+// RCS-ID:      
+// Copyright:   
+// Licence:     
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _PROFOPTIONSDIALOG_H_
+#define _PROFOPTIONSDIALOG_H_
+
+#ifdef __GNUG__
+#pragma interface "ProfOptionsDialog.h"
+#endif
+
+/*!
+ * Includes
+ */
+
+////@begin includes
+////@end includes
+
+/*!
+ * Forward declarations
+ */
+
+////@begin forward declarations
+////@end forward declarations
+
+/*!
+ * Control identifiers
+ */
+
+////@begin control identifiers
+#define ID_DIALOG 10000
+#define ID_CHECKLISTBOX 10001
+#define ID_TXTCODE 10002
+#define ID_RANDOMIZE 10004
+#define ID_GENERATE 10003
+#define ID_EXITBUTTON 10005
+////@end control identifiers
+
+/*!
+ * ProfOptionsDialog class declaration
+ */
+
+class ProfOptionsDialog: public wxDialog
+{    
+    DECLARE_CLASS( ProfOptionsDialog )
+    DECLARE_EVENT_TABLE()
+
+public:
+    /// Constructors
+    ProfOptionsDialog( );
+    ProfOptionsDialog( wxWindow* parent, wxWindowID id = -1, const wxString& caption = _("Chameleon Options Manager"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxCAPTION|wxRESIZE_BORDER|wxSYSTEM_MENU );
+
+    /// Creation
+    bool Create( wxWindow* parent, wxWindowID id = -1, const wxString& caption = _("Chameleon Options Manager"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxCAPTION|wxRESIZE_BORDER|wxSYSTEM_MENU );
+
+    /// Creates the controls and sizers
+    void CreateControls();
+
+////@begin ProfOptionsDialog event handler declarations
+    /// wxEVT_COMMAND_BUTTON_CLICKED event handler for ID_GENERATE
+    void OnGenerateClick( wxCommandEvent& event );
+
+    /// wxEVT_COMMAND_BUTTON_CLICKED event handler for ID_EXITBUTTON
+    void OnExitbuttonClick( wxCommandEvent& event );
+
+////@end ProfOptionsDialog event handler declarations
+
+////@begin ProfOptionsDialog member function declarations
+////@end ProfOptionsDialog member function declarations
+
+    /// Should we show tooltips?
+    static bool ShowToolTips();
+
+////@begin ProfOptionsDialog member variables
+    wxCheckListBox* m_chklstModules;
+    wxTextCtrl* m_txtGeneratedCode;
+    wxCheckBox* m_chkRandomize;
+    wxButton* m_genButton;
+    wxButton* m_exitButton;
+////@end ProfOptionsDialog member variables
+};
+
+#endif
+    // _PROFOPTIONSDIALOG_H_
